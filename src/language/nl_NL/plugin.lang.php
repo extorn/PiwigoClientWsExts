31889b75a316aa560775df62e83e27edc70d383f
fly-man- <fly.man.opensim@gmail.com>
2020-10-03 23:32:10 +0200

    Dutch localisation

---
+++ src/language/nl_NL/plugin.lang.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * as much as possible prefer using comprehensive keys (such as "Check here") instead of
+ * formatted keys (eg "PiwigoClientWsExts_check_button"), it would make your code more readable and help
+ * the translation team as well
+ */
+
+$lang['PiwigoClientWsExts'] = 'PiwigoClientWsExts';
+$lang['What PiwigoClientWsExts can do for me?'] = 'Wat kan PiwigoClientWsExts voor mij betekenen?';
+
+?>
